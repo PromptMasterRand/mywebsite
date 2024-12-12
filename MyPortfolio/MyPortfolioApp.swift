96c0533938aaa5f0c542045980c0491ddeba817c
Randon McCrea <randonmccrea@randons-air.mynetworksettings.com>
2024-12-12 12:22:39 -0500

    Add Helpdesk Ticketing System page

---
+++ MyPortfolio/MyPortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyPortfolioApp.swift
+//  MyPortfolio
+//
+//  Created by Randon McCrea on 12/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MyPortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
